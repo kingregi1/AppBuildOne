5351bc2598f34821ea3724c71bfd342e3dce3197
kingregi1 <59853870+kingregi1@users.noreply.github.com>
2020-02-13 10:43:21 -1000

    Adding Labels About UH

--- AppBuildOne/ViewController.swift
+++ AppBuildOne/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    @IBOutlet var firstlabel: UILabel!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+self.firstlabel.text = "University Of Hawaii Oahu"
     }
 
 
-}
 
+
+}
