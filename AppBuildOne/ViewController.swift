a116ec487ae4d80959fd9fa7915eb58962e0a4da
kingregi1 <59853870+kingregi1@users.noreply.github.com>
2020-02-25 10:53:48 -1000

    first view step 6

--- AppBuildOne/ViewController.swift
+++ AppBuildOne/ViewController.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var firstlabel: UILabel!
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    
+    @IBOutlet var textView: UITextView!
+    
+    
+    
+    
+    override func viewDidLoad() {
         
-self.firstlabel.text = "University Of Hawaii Oahu"
+        self.textView.text = "Since its, inception in, 1976, the University of Hawaiʻi   West Oʻahu has been committed to delivering accessible, affordable higher education to residents on the Leeward Coast through dynamic, _: flexible, and responsive methods.UH West Oʻahu moved to its current campus in 2012 and continues to construct new facilities to meet the needs of its growing enrollment.The university strives to be a good community partner by meeting the educational requirements of its community by providing educational programs responsive to the local job market"
+
+        UH West Oʻahu offers unique programs such as creative media,: cybersecurity, health sciences, hospitality and tourism, and sustainable community food systems   developed to address workforce needs and leverage today s technologies to train the next generation of community leaders)
+    self.firstlabel.text = "University Of Hawaii Oahu"
+        super.viewDidLoad()
+        // Do any additional setup after loading the }
+
+}
+    
+    @IBAction func aboutACMButtonPressed(_ sender: Any) {
+        self.textView.text = 
+        
+        
     }
-
-
-
-
+    
+    
 }
