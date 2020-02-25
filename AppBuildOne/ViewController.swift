413dbf627ad37be51a85149e5206ee4249f0f99a
kingregi1 <59853870+kingregi1@users.noreply.github.com>
2020-02-25 11:19:17 -1000

    step 6 first view completed

--- AppBuildOne/ViewController.swift
+++ AppBuildOne/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
         
         self.textView.text = "Since its, inception in, 1976, the University of Hawaiʻi   West Oʻahu has been committed to delivering accessible, affordable higher education to residents on the Leeward Coast through dynamic, _: flexible, and responsive methods.UH West Oʻahu moved to its current campus in 2012 and continues to construct new facilities to meet the needs of its growing enrollment.The university strives to be a good community partner by meeting the educational requirements of its community by providing educational programs responsive to the local job market"
 
-        UH West Oʻahu offers unique programs such as creative media,: cybersecurity, health sciences, hospitality and tourism, and sustainable community food systems   developed to address workforce needs and leverage today s technologies to train the next generation of community leaders)
+        
     self.firstlabel.text = "University Of Hawaii Oahu"
         super.viewDidLoad()
         // Do any additional setup after loading the }
@@ -29,10 +29,14 @@ class ViewController: UIViewController {
 }
     
     @IBAction func aboutACMButtonPressed(_ sender: Any) {
-        self.textView.text = 
-        
+        self.textView.text="UH West Oʻahu offers unique programs such as creative media,: cybersecurity, health sciences, hospitality and tourism, and sustainable community food systems   developed to address workforce needs and leverage today s technologies to train the next generation of community leaders)"
+         self.firstlabel.text="About ACM"
         
     }
     
+    @IBAction func UHWObuttonPressed(_ sender: Any) {
+        self.textView.text = "Since its, inception in, 1976, the University of Hawaiʻi   West Oʻahu has been committed to delivering accessible, affordable higher education to residents on the Leeward Coast through dynamic, _: flexible, and responsive methods.UH West Oʻahu moved to its current campus in 2012 and continues to construct new facilities to meet the needs of its growing enrollment.The university strives to be a good community partner by meeting the educational requirements of its community by providing educational programs responsive to the local job market"
+        self.firstlabel.text="UHWO"
+    }
     
 }
